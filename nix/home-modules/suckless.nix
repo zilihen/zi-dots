2cb19c7363d18f7093c2f59d823c57fc9c77b776
Jason Chen <jasonchen7766@gmail.com>
2025-12-31 03:28:59 -0500

    Add suckless.nix for st and dmenu configurations

---
+++ nix/home-modules/suckless.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    (pkgs.st.overrideAttrs (_: {
+      src = ../config/st;
+      patches = [ ];
+    }))
+    (pkgs.dmenu.overrideAttrs (_: {
+      src = ../config/dmenu;
+      patches = [ ];
+    }))
+  ];
+}
