62d518de1021bab887e01557b9ca60690bbe6cb9
zi <jasonchen7766@gmail.com>
2026-06-06 22:51:19 -0400

    adjusted some setting

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -1,8 +1,7 @@
-
 -- autostart
-hl.on("hyprland.start", function () 
-  hl.exec_cmd("udiskie")
-  hl.exec_cmd("wayle panel start & hyprpaper & hypridle")
+hl.on("hyprland.start", function()
+    hl.exec_cmd("udiskie")
+    hl.exec_cmd("wayle panel start & hyprpaper & hypridle")
 end)
 
 -- env variables
@@ -53,7 +52,7 @@ hl.bind("SUPER + CTRL + Up", hl.dsp.window.swap({ direction = "up" }))
 hl.bind("SUPER + CTRL + Down", hl.dsp.window.swap({ direction = "down" }))
 
 hl.bind("ALT + Tab", function()
-    hl.dispatch(hl.dsp.window.cycle_next())    -- Change focus to another window
+    hl.dispatch(hl.dsp.window.cycle_next())   -- Change focus to another window
     hl.dispatch(hl.dsp.window.bring_to_top()) -- Bring it to the top
 end)
 
@@ -90,18 +89,22 @@ hl.bind("SUPER + mouse:273", hl.dsp.window.resize(), {
 })
 
 -- Laptop Volume Keybinds
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
-hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
-hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
+    { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
+    { locked = true, repeating = true })
+hl.bind("XF86AudioMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
+    { locked = true, repeating = true })
+hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
+    { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
 
 -- Media control
-hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
 
 -- Monitor
 hl.monitor({
@@ -116,24 +119,24 @@ hl.monitor({
 
 hl.config({
     input = {
-        kb_layout  = "us",
-        kb_variant = "",
-        kb_model   = "",
-        kb_options = "",
-        kb_rules   = "",
+        kb_layout          = "us",
+        kb_variant         = "",
+        kb_model           = "",
+        kb_options         = "",
+        kb_rules           = "",
         numlock_by_default = true,
-        repeat_rate = 25, 
-        repeat_delay = 250,
+        repeat_rate        = 25,
+        repeat_delay       = 250,
 
-        follow_mouse = 1,
+        follow_mouse       = 1,
 
-        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
-        accel_profile = "adaptive",
+        sensitivity        = 0, -- -1.0 - 1.0, 0 means no modification.
+        accel_profile      = "adaptive",
 
-        touchpad = {
+        touchpad           = {
             natural_scroll = true,
-            tap_to_click = true, 
-            tap_and_drag = true, 
+            tap_to_click = true,
+            tap_and_drag = true,
             scroll_factor = 1,
             clickfinger_behavior = true
         },
@@ -149,13 +152,13 @@ hl.gesture({
 -- General Setting and Decoration
 hl.config({
     general = {
-        gaps_in  = 2,
-        gaps_out = 8,
+        gaps_in          = 2,
+        gaps_out         = 8,
 
-        border_size = 2,
+        border_size      = 2,
 
-        col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+        col              = {
+            active_border   = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
             inactive_border = "rgba(595959aa)",
         },
 
@@ -163,31 +166,35 @@ hl.config({
         resize_on_border = true,
 
         -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
-        allow_tearing = false,
+        allow_tearing    = false,
 
-        layout = "dwindle",
+        layout           = "dwindle",
+    },
+
+    xwayland = {
+        force_zero_scaling = true
     },
 
     decoration = {
-        rounding       = 10,
-        rounding_power = 2,
+        rounding         = 10,
+        rounding_power   = 2,
 
         -- Change transparency of focused and unfocused windows
         active_opacity   = 0.95,
         inactive_opacity = 0.9,
 
-        shadow = {
+        shadow           = {
             enabled      = false,
             range        = 4,
             render_power = 3,
             color        = 0xee1a1a1a,
         },
 
-        blur = {
-            enabled   = true,
-            size      = 3,
-            passes    = 1,
-            vibrancy  = 0.1696,
+        blur             = {
+            enabled  = true,
+            size     = 3,
+            passes   = 1,
+            vibrancy = 0.1696,
         },
     },
 
@@ -196,7 +203,7 @@ hl.config({
     },
 
     dwindle = {
-        preserve_split = true, 
+        preserve_split = true,
     },
 })
 
@@ -204,47 +211,47 @@ hl.config({
 -- Animation Setting
 
 -- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
-hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
-hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
-hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
-hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}    } })
-hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
+hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
+hl.curve("easeInOutCubic", { type = "bezier", points = { { 0.65, 0.05 }, { 0.36, 1 } } })
+hl.curve("linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
+hl.curve("almostLinear", { type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } })
+hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
 
 -- Default springs
-hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+hl.curve("easy", { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
 
-hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
-hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
-hl.animation({ leaf = "windows",       enabled = true,  speed = 4.79, spring = "easy" })
-hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  spring = "easy",         style = "popin 87%" })
-hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 87%" })
-hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" })
-hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" })
-hl.animation({ leaf = "layers",        enabled = true,  speed = 3.81, bezier = "easeOutQuint" })
-hl.animation({ leaf = "layersIn",      enabled = true,  speed = 4,    bezier = "easeOutQuint", style = "fade" })
-hl.animation({ leaf = "layersOut",     enabled = true,  speed = 1.5,  bezier = "linear",       style = "fade" })
-hl.animation({ leaf = "fadeLayersIn",  enabled = true,  speed = 1.79, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeLayersOut", enabled = true,  speed = 1.39, bezier = "almostLinear" })
-hl.animation({ leaf = "workspaces",    enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 1.21, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 7,    bezier = "quick" })
+hl.animation({ leaf = "global", enabled = true, speed = 10, bezier = "default" })
+hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
+hl.animation({ leaf = "windows", enabled = true, speed = 4.79, spring = "easy" })
+hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.1, spring = "easy", style = "popin 87%" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 1.49, bezier = "linear", style = "popin 87%" })
+hl.animation({ leaf = "fadeIn", enabled = true, speed = 1.73, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeOut", enabled = true, speed = 1.46, bezier = "almostLinear" })
+hl.animation({ leaf = "fade", enabled = true, speed = 3.03, bezier = "quick" })
+hl.animation({ leaf = "layers", enabled = true, speed = 3.81, bezier = "easeOutQuint" })
+hl.animation({ leaf = "layersIn", enabled = true, speed = 4, bezier = "easeOutQuint", style = "fade" })
+hl.animation({ leaf = "layersOut", enabled = true, speed = 1.5, bezier = "linear", style = "fade" })
+hl.animation({ leaf = "fadeLayersIn", enabled = true, speed = 1.79, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 1.39, bezier = "almostLinear" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesIn", enabled = true, speed = 1.21, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesOut", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "zoomFactor", enabled = true, speed = 7, bezier = "quick" })
 
 
 -- Rules
 hl.window_rule({
     -- Ignore maximize requests from all apps. You'll probably like this.
-    name  = "suppress-maximize-events",
-    match = { class = ".*" },
+    name           = "suppress-maximize-events",
+    match          = { class = ".*" },
 
     suppress_event = "maximize",
 })
 
 hl.window_rule({
     -- Fix some dragging issues with XWayland
-    name  = "fix-xwayland-drags",
-    match = {
+    name     = "fix-xwayland-drags",
+    match    = {
         class      = "^$",
         title      = "^$",
         xwayland   = true,
@@ -265,44 +272,43 @@ hl.window_rule({
     float = true,
 })
 
-hl.window_rule({ 
-    name = "Launcher", 
-    match = { 
+hl.window_rule({
+    name = "Launcher",
+    match = {
         class = "(steam)|(heroic)|(org.prismlauncher.PrismLauncher)"
     },
-    float = true, 
-    workspace = 4, 
-    center = true,
-    no_initial_focus = true,
-})
-
-hl.window_rule({ 
-    name = "discord", 
-    match = { 
-        class = "discord"
-    }, 
-    workspace = 3, 
-    no_initial_focus = true,
-})
-
-hl.window_rule({ 
-    name = "games", 
-    match = { 
-        class = "^steam_app_[0-9]*$",
-    }, 
-    workspace = 4,
-    float = true, 
-    no_initial_focus = true, 
-    size = {"monitor_w * 0.75", "monitor_h * 0.6"}, 
-    center = true,
-})
-
-hl.window_rule({ 
-    name = "float layout workspace 4", 
-    match = { 
-        workspace = 4,
-    }, 
     float = true,
-    size = {"monitor_w * 0.75", "monitor_h * 0.6"}
+    workspace = 4,
+    center = true,
+    no_initial_focus = true,
 })
 
+hl.window_rule({
+    name = "discord",
+    match = {
+        class = "discord"
+    },
+    workspace = 3,
+    no_initial_focus = true,
+})
+
+hl.window_rule({
+    name = "games",
+    match = {
+        class = "^steam_app_[0-9]*$",
+    },
+    workspace = 4,
+    float = true,
+    no_initial_focus = true,
+    size = { "monitor_w * 0.75", "monitor_h * 0.6" },
+    center = true,
+})
+
+hl.window_rule({
+    name = "float layout workspace 4",
+    match = {
+        workspace = 4,
+    },
+    float = true,
+    size = { "monitor_w * 0.75", "monitor_h * 0.6" }
+})
